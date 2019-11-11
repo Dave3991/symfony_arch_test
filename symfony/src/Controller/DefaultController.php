4e7245f751391f62a8aed983c11dec4fdce5c424
David Bauer <bauer@logio.cz>
2019-11-11 12:25:44 +0100

    initial commit

---
+++ symfony/src/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+class DefaultController extends AbstractController
+{
+
+    /**
+     * @Route("", name="default")
+     */
+    public function defaultAction()
+    {
+        return new Response("test");
+    }
+}
